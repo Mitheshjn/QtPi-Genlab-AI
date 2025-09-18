24ee7779bcce88e107d62a21d4fee12e7c74e9df
Mitheshjn <72849423+Mitheshjn@users.noreply.github.com>
2025-09-18 14:55:14 +0530

    test upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from uqtpy.comm.ble.service import QtBLE
+from uqtpy.comm.ble.service import QtBLEService
+
+from uqtpy.actuators.motor import Motor
+from uqtpy.actuators.argb import ARGB
+from uqtpy.actuators.buzzer import Buzzer
+from uqtpy.actuators.servo import Servo
+from uqtpy.sensors.ultrasonic import UltraSonic
+from uqtpy.sensors.tof import TOF
+
+#mpu = MPU6050(ob=True)
+#apds = APDS(ob=True)
+motor = Motor(ob=True)
+argb = ARGB(ob=True)
+buzzer = Buzzer(ob=True)
+servoObj = Servo(ob=True)
+us = UltraSonic(ob=True)
+tof = TOF(ob=True)
+
+components = None
+qtservice = None
+#components = [mpu, apds, motor, argb,buzzer, servoObj, us, tof]
+components = [ buzzer, motor, servoObj]
+qtservice = QtBLEService(components=components,uuid='0x1011')
+
+qtble = QtBLE(name='qtble', debug=True)
+qtble.add_services(services=[qtservice])
+qtble.start_advertising()
+#qtble.start_services(read_freq=100)
